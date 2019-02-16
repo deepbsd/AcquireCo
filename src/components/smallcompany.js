8ff0715dee320d7170a0280f89da5aa03ae92daf
David S. Jackson <deepbsd@yahoo.com>
2019-02-16 09:12:07 -0500

    started new branch 'newSchema' I think it is.  Starting to fill out financial schema

--- src/components/smallcompany.js
+++ src/components/smallcompany.js
@@ -6,7 +6,16 @@ export default function Smallcompany(props){
         <div className="indivCompany">
             <h2>{props.name}</h2>
             <h4>contact: {props.contact}</h4>
-            <p>financials: {props.financials}</p>
+            <p>email: {props.contactEmail} phone: {props.contactPhone}</p>
+            <h4>financials:</h4>
+            <p>
+              S&P Quality: {props.SPValue} TD/CA: {props.DebtToCA} 
+              CR: {props.CR} 5YR EPS Growth: {props.fiveYrEPSGrowth} 
+            </p>
+            <p>
+              P/E: {props.PE}  P/BV: {props.PBV} DivPS: {props.DivPS}
+            </p>
+            <p>old: {props.financials}</p>
             <p>status: {props.status}</p>
         </div>
     );
